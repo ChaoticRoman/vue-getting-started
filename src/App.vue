02b85d7c6fe7b5d4e781b4aac8b54036cc3079a5
Pavelka, Roman (ADV D EU CZ PDS1 CIO 1) <roman.pavelka@siemens.com>
2023-12-12 14:40:53 +0100

    Add an example of computed property

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+
 const greeting = ref("How are you going, my friend?");
+
+const x = ref(5);
+
+const computed_example = computed(() => {
+  return x.value * x.value;
+});
 </script>
 
 <template>
   <div id="app">
     <h1>Hello, World!</h1>
     {{ greeting }}
+    <p />
+    Square of {{ x }} is {{ computed_example }}.
   </div>
 </template>
 
