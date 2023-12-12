4f7784d819e642671c8a6d4d52f4bbf2f6cd6f52
Pavelka, Roman (ADV D EU CZ PDS1 CIO 1) <roman.pavelka@siemens.com>
2023-12-12 13:04:29 +0100

    Playing with reacitity

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
+<script>
+
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+
+  setup() {
+    return {
+      greeting
+    }
+  }
+}
+
+const greeting = ref("How are you going, my friend?");
+
+</script>
+
 <template>
   <div id="app">
-    <h1>Hello, Worlds!</h1>
+    <h1>Hello, World!</h1>
+    {{ greeting }}
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
 
 <style>
 #app {
